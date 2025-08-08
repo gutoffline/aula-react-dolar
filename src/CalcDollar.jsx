7fb3aaddb7b9dcbc56abfed4bd58dcd832f427ca
Guto Xavier <gutoffline@gmail.com>
2025-08-07 21:48:49 -0300

    icones

--- src/CalcDollar.jsx
+++ src/CalcDollar.jsx
@@ -1,26 +1,28 @@
 import './CalcDollar.css'
+import { RiMoneyDollarCircleFill } from "react-icons/ri";
+
 
 function CalcDollar() {
 
     const calcular = (function () {
         const reais = document.getElementById("reais").value
         const cotacao = document.getElementById("cotacao").value
-        total = reais * cotacao
-        alert(cotacao)
+        const total = reais / cotacao
+        alert(total)
     })
 
     return (
         <>
             <form className='w-25 m-auto pt-5 pb-5'>
-                <div class="form-group  mb-3">
+                <div className="form-group  mb-3">
                     <label htmlFor="reais">Valor em Reais</label>
                     <input type="text" className="form-control" id="reais" />
                 </div>
-                <div class="form-group mb-3">
+                <div className="form-group mb-3">
                     <label htmlFor="cotacao">Cotação do Dólar</label>
                     <input type="text" className="form-control" id="cotacao" />
                 </div>
-                <button type="button" onClick={calcular} className="btn btn-primary w-100">Calcular</button>
+                <button type="button" onClick={calcular} className="btn btn-primary w-100"> <RiMoneyDollarCircleFill size={20} color='white' /> Calcular</button>
             </form>
         </>
     )
